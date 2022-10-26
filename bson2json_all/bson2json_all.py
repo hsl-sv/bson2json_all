beac5932c8f821790180682143abcad9fd1ef3bc
hsl-sv <flt@kakao.com>
2022-10-26 16:33:25 +0900

    bson 삭제 추가, compare 제거

--- bson2json_all/bson2json_all.py
+++ bson2json_all/bson2json_all.py
@@ -1,13 +1,16 @@
 import glob
 import json
+import os
 import bson
 
-flist = glob.glob('D:/UWO/unpack/NonUFS_221015/cms/**/*.bson', recursive=True)
+flist = glob.glob('D:/UWO/unpack/NonUFS_221026/cms/**/*.bson', recursive=True)
 
 for i, bsf in enumerate(flist):
     with open(bsf,'rb') as f:
         data = bson.decode_all(f.read())
         f.close()
+
+    os.remove(bsf)
 
     with open(bsf + '.json', "w", encoding='utf-8') as fw:
         json.dump(data, fw, indent=4, sort_keys=True, ensure_ascii=False)
